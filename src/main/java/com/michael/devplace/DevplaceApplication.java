e201129824ef6ab257900294bd527a850f6cb2df
kangmoonsu <moonsu1011@naver.com>
2023-10-21 18:26:00 +0900

    Initial commit / 프로젝트 초기 설정완료

---
+++ src/main/java/com/michael/devplace/DevplaceApplication.java
@@ -0,0 +1,13 @@
+package com.michael.devplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/michael/devplace/DevplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.devplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
